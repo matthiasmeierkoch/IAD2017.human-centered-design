95e1039d555c5229bf40039855c920a08e94aff1
Matthias Koch <matthiasmeierkoch@hotmail.com>
2019-03-23 08:38:27 +0100

    bocado back

---
+++ docs/bocado/src/js/index.js
@@ -0,0 +1,14 @@
+import 'bootstrap';
+// return UserList class
+class Test {
+  constructor(name) {
+    this.name = name;
+  }
+
+  // initialize plugin
+  log() {
+    console.log(this.name);
+  }
+}
+
+export default Test;
